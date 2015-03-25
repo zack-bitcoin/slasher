5337bfe93cac84275306f0e4c5fcfd1ab35910d4
zack <zack.bitcoin@gmail.com>
2015-03-24 17:34:01 -0700

    security explanation update

--- security.py
+++ security.py
@@ -1,46 +1,62 @@
-"""
-Scenario
-Say there are 100 validators per block. 43% of them want to collude to maintain a fork. On the blocks where they control over 67%, they can create the block on the wrong fork.
-
-odds that out of 100 random draws, 0 are validators:
-0.67**100
-odds that out of 100 random draws, 1 are validators:
-0.43*0.57**99*100
-
-The odds that the attackers control a block?
-1-(0.43^0*0.57^100*(choose 100 0))-...-(0.43**67*0.57**33*(choose 100 57))
-"""
 import math, numpy
-def fact(x): return math.factorial(x)
-def choose(b, a): return fact(b)/(1.0*fact(a)*fact(b-a))
-def sub_range(total, attackers, r):
-    p=1.0*attackers/total
-    q=1-p#prob that a random validator is not an attacker.
-    times=int(total*r)
-    return sub_range_helper(total, attackers, p, q, times)
-def sub_range_helper(total, attackers, p, q, times):
-    if attackers>total: 1
-    if times<1: return 0
-    a=choose(total, times)*(p**times)*(q**(total-times))
-    return a+sub_range_helper(total, attackers, p, q, times-1)
-def prob_control_block(total, attackers, p):#if there are exactly total validators selected to control the next block and each has a attackers/100 probability of being an attacker, then this is the probability they will control >p% of the next block.
-    return 1-(sub_range(total, attackers, p))
-def gamma_control_block(total, attackers, p):# if each person has a small possibility to be a validator, and there are total validators on average, and attackers/100 is the percentage of people who want to attack, then this outputs the probability that the attackers will control >p of the next block
-    a=0
-    rounds=5000
+from cdecimal import Decimal
+def control_block_trial(total, clique, p):
+    if numpy.random.poisson(clique)>p*total:
+        return 1
+    return 0
+def control_block_probability(clique, total=Decimal(78), p=Decimal('0.66666666666666666666666666666666666666666666666666666')):
+    #how often does the clique, which is a subset of the total, control > p of the block?
+    a=Decimal(0)
+    rounds=Decimal(500000)
     for i in range(rounds):
-        a+=prob_control_block(int(numpy.random.gamma(total, 1)), attackers, p)
-    return(a/rounds)
-def gamma_freeze(attackers):
-    return gamma_control_block(100, 100-attackers, 2.0/3)
-def gamma_fork(attackers):
-    return gamma_control_block(100, attackers, 2.0/3)
+        a+=control_block_trial(total, clique, p)
+    print("a: " +str(a))
+    return(Decimal(a)/rounds)
+def frequency_half_control(x): return Decimal(1)/control_block_probability(x/2, x)
 def test():
-    print("given that X% of the validators want to maintain a fork, how often will they control more than 67% of the next block. 100 validators total selected")
-    print("assuming that a gap of size 50 is too big to jump")
-    print(gamma_freeze(49.7))#0.02
-    print("this shows that any 49.7% of validators could freeze the network")
-    print(gamma_fork(50.3))#0.02
-    print("this shows that any 50.3% of validators could maintain a fork the network")
+    #We are aiming for a clique of 50% of the bonded validators to be able to verify 4% of the blocks. verification requres 2/3 of all signatures.
+    j=54
+    print(str(j)+" : " +str(frequency_half_control(Decimal(j))))
+    print("this shows that if there are 54 validators on average, then an attacking group with 50% of bonded stake would control 1 block in 25.7 on average")
+def proof_gap():
+    #gap is given by numpy.random.gamma(1, 50)
+    import random
+    def f(): return random.random()*25>24
+    def g(): return "1" if f() else "0"
+    def h():
+        out=""
+        for i in range(1000):
+            out+=g()
+        out=out.split("1")
+        #print(out)
+        out=map(lambda x: len(x), out)
+        return out
+    #h() =doing=same= gamma()
 
+    def gamma():
+        out=[]
+        for i in range(25):
+            out+=[int(numpy.random.gamma(1, 50))]
+        return out
 
+def fork_time():#how long till fork dies? assuming 50% want the fork to live.
+    #assuming 50 blocks is too big of a gap to jump.
+    #this shows that that we should aim for a 4% probability that attackers can control the block. (1/25 = 0.04)
+    #forks usually die within 200 blocks.
+    def tg():#probability next gap is big enough to kill
+        out=0
+        times=1000
+        for i in range(times):
+            if numpy.random.gamma(1, 25)>50:
+                out+=1
+        return out*1.0/times
+    def fg():#average gap size
+        out=0
+        times=1000
+        for i in range(times):
+            out+=numpy.random.gamma(1, 25)
+        return out*1.0/times
+    return fg()*(1.0/tg())#~180
+if __name__=="__main__":
+    print(test())
+    #print(gamma())
